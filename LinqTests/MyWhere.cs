01b019a67a5805ca8ab246df957ee883050591cb
lulu.li29 <s987337613@gmail.com>
2018-04-28 18:05:54 +0800

    add distinct

---
+++ LinqTests/MyWhere.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace LinqTests
+{
+    public static class MyWhere
+    {
+        public static IEnumerable<TSource> MyOwnWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (func(enumerator.Current))
+                {
+                    yield return enumerator.Current;
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> MyTake<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            var enumerator = source.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                if (index < count)
+                {
+                    yield return enumerator.Current;
+                    index++;
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+        public static IEnumerable<TSource> MySkip<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            var enumerator = source.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                if (index < count)
+                {
+                    index++;
+                }
+                else
+                {
+                    yield return enumerator.Current;
+                }
+            }
+        }
+        public static IEnumerable<TSource> MySkipWhile<TSource>(this IEnumerable<TSource> source, int count, Func<TSource, bool> func)
+        {
+            var enumerator = source.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                if (func(enumerator.Current) && index < count)
+                {
+                    index++;
+                }
+                else
+                {
+                    yield return enumerator.Current;
+                }
+            }
+        }
+        public static IEnumerable<TSource> MyTakeWhile<TSource>(this IEnumerable<TSource> source, int count, Func<TSource, bool> func)
+        {
+            var enumerator = source.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                if (func(enumerator.Current))
+                {
+                    if (index >= count)
+                    {
+                        yield break;
+                    }
+
+                    yield return enumerator.Current;
+                    index++;
+                }
+            }
+        }
+        public static IEnumerable<TSource> MyRealTakeWhile<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (func(enumerator.Current))
+                {
+                    yield return enumerator.Current;
+
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+        public static int MySum<TSource>(this IEnumerable<TSource> source, Func<TSource, int> func)
+        {
+            var enumerator = source.GetEnumerator();
+            var sum = 0;
+            while (enumerator.MoveNext())
+            {
+                sum += func(enumerator.Current);
+            }
+            return sum;
+        }
+        public static IEnumerable<int> MySum<TSource>(this IEnumerable<TSource> source, int count, Func<TSource, int> func)
+        {
+            var enumerator = source.GetEnumerator();
+            var sum = 0;
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                index++;
+                sum += func(enumerator.Current);
+                if (index == count)
+                {
+                    yield return sum;
+                    sum = 0;
+                    index = 0;
+                }
+
+            }
+            yield return sum;
+        }
+        public static bool MyAny<TSource>(this IEnumerable<TSource> source)
+        {
+            var enumerator = source.GetEnumerator();
+
+            return enumerator.MoveNext();
+            
+        }
+        public static bool MyAll<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
+        {
+            var r = source.GetEnumerator();
+            while (r.MoveNext())
+            {
+                if (func(r.Current))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static List<TSource> MyDistinct<TSource>(this IEnumerable<TSource> source)
+        {
+            var r = source.GetEnumerator();
+            
+            var result = new List<TSource>();
+            while (r.MoveNext())
+            {
+                if (!result.Contains(r.Current))
+                {
+                    result.Add(r.Current);
+                }
+            }
+            return result;
+        }
+        
+    }
+}
--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using ExpectedObjects;
-using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LinqTests
@@ -10,20 +9,20 @@ namespace LinqTests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void find_products_that_price_between_200_and_500()
-        {
-            var products = RepositoryFactory.GetProducts();
-            var actual = WithoutLinq.FindProductByPrice(products, 200, 500, "Odd-e");
+        //[TestMethod]
+        //public void find_products_that_price_between_200_and_500()
+        //{
+        //    var products = RepositoryFactory.GetProducts();
+        //    var actual = WithoutLinq.FindProductByPrice(products, 200, 500, "Odd-e");
 
-            var expected = new List<Product>()
-            {
-                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
-                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
-            };
+        //    var expected = new List<Product>()
+        //    {
+        //        new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+        //        new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"}
+        //    };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
-        }
+        //    expected.ToExpectedObject().ShouldEqual(actual);
+        //}
 
         [TestMethod]
         public void myLinq()
@@ -33,8 +32,8 @@ namespace LinqTests
 
             var expected = new List<Product>()
             {
-                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
-                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"}
             };
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
@@ -48,10 +47,10 @@ namespace LinqTests
 
             var expected = new List<Employee>()
             {
-                new Employee{Name="Tom", Role=RoleType.Engineer, MonthSalary=140, Age=33, WorkingYear=2.6} ,
-                new Employee{Name="Bas", Role=RoleType.Engineer, MonthSalary=280, Age=36, WorkingYear=2.6} ,
-                new Employee{Name="Mary", Role=RoleType.OP, MonthSalary=180, Age=26, WorkingYear=2.6} ,
-                new Employee{Name="Joey", Role=RoleType.Engineer, MonthSalary=250, Age=40, WorkingYear=2.6},
+                new Employee {Name = "Tom", Role = RoleType.Engineer, MonthSalary = 140, Age = 33, WorkingYear = 2.6},
+                new Employee {Name = "Bas", Role = RoleType.Engineer, MonthSalary = 280, Age = 36, WorkingYear = 2.6},
+                new Employee {Name = "Mary", Role = RoleType.OP, MonthSalary = 180, Age = 26, WorkingYear = 2.6},
+                new Employee {Name = "Joey", Role = RoleType.Engineer, MonthSalary = 250, Age = 40, WorkingYear = 2.6},
             };
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
@@ -97,7 +96,196 @@ namespace LinqTests
 
             var expected = new List<int>()
             {
-                19,20,19,17
+                19,
+                20,
+                19,
+                17
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void GetEmployeesSalary()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MyOwnWhere(e => e.Role == RoleType.Engineer).MySelect(e => e.MonthSalary);
+
+            var expected = new List<int>()
+            {
+                100,
+                140,
+                280,
+                120,
+                250
+            };
+
+            foreach (var VARIABLE in actual)
+            {
+                Console.WriteLine();
+            }
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void MyTake()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MyTake(3);
+
+            var expected = new List<Product>()
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"}
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void MySelect()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MyTake(3).MySelect((x, index) => $"{index + 1}-{x.Name}");
+
+            var expected = new List<string>()
+            {
+                "1-Joe",
+                "2-Tom",
+                "3-Kevin"
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void MySkip()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MySkip(6);
+
+            var expected = new List<Employee>()
+            {
+                new Employee {Name = "Frank", Role = RoleType.Engineer, MonthSalary = 120, Age = 16, WorkingYear = 2.6},
+                new Employee {Name = "Joey", Role = RoleType.Engineer, MonthSalary = 250, Age = 40, WorkingYear = 2.6},
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void MySkipWhile()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MySkipWhile(4, p => p.Price > 300);
+
+            var expected = new List<Product>()
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"},
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void MyTakeWhile()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MyTakeWhile(2, p => p.Price > 300);
+
+            var expected = new List<Product>()
+            {
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void Real_TakeWhile()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MyRealTakeWhile(p => p.Age > 30);
+
+            var expected = new List<Employee>()
+            {
+                new Employee {Name = "Joe", Role = RoleType.Engineer, MonthSalary = 100, Age = 44, WorkingYear = 2.6},
+                new Employee {Name = "Tom", Role = RoleType.Engineer, MonthSalary = 140, Age = 33, WorkingYear = 2.6},
+                new Employee {Name = "Kevin", Role = RoleType.Manager, MonthSalary = 380, Age = 55, WorkingYear = 2.6},
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void sum()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MySum(p => p.Price);
+
+            var expected = 3650;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void sumGroup3()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MySum(3, p => p.Price);
+
+            var expected = new List<int>
+            {
+                630,
+                1530,
+                1490
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void sumGroup5()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MySum(5, p => p.Price);
+
+            var expected = new List<int>
+            {
+                1550,
+                2100
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void any()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MyAny();
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void all()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MyAll(x => x.Price > 200);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void distinct()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.Select(x => x.Role).MyDistinct();
+            var expected = new List<RoleType>
+            {
+                RoleType.Engineer,
+                RoleType.Manager,
+                RoleType.OP
             };
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
@@ -113,6 +301,15 @@ namespace LinqTests
             }
         }
 
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> sources, Func<TSource, int, TResult> selector)
+        {
+            var index = 0;
+            foreach (var item in sources)
+            {
+                yield return selector(item, index++);
+            }
+        }
+
         public static IEnumerable<string> AddPort(IEnumerable<string> urls)
         {
             foreach (var url in urls)
@@ -123,59 +320,35 @@ namespace LinqTests
     }
 }
 
-internal class WithoutLinq
-{
-    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary, string supplier)
-    {
-        var list = new List<Product>();
-        foreach (var product in products)
-        {
-            if (product.Price > lowBoundary && product.Price < highBoundary && product.Supplier == supplier)
-            {
-                list.Add(product);
-            }
-        }
+//internal class WithoutLinq
+//{
+//    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary,
+//        string supplier)
+//    {
+//        var list = new List<Product>();
+//        foreach (var product in products)
+//        {
+//            if (product.Price > lowBoundary && product.Price < highBoundary && product.Supplier == supplier)
+//            {
+//                list.Add(product);
+//            }
+//        }
 
-        return list;
-    }
-}
+//        return list;
+//    }
+//}
 
-internal static class YourOwnLinq
-{
-    public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func, IEnumerable<TSource> products)
-    {
-        var enumerator = products.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            if (func(enumerator.Current))
-            {
-                yield return enumerator.Current;
-            }
-        }
-        //var list = new List<Product>();
-        //foreach (var product in products)
-        //{
-        //    if (func(product))
-        //    {
-        //        list.Add(product);
-        //    }
-        //}
-
-        //return list;
-    }
-}
-
-public static class MyWhere
-{
-    public static IEnumerable<TSource> MyOwnWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
-    {
-        var enumerator = source.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            if (func(enumerator.Current))
-            {
-                yield return enumerator.Current;
-            }
-        }
-    }
-}
+//internal static class YourOwnLinq
+//{
+//    public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func, IEnumerable<TSource> products)
+//    {
+//        var enumerator = products.GetEnumerator();
+//        while (enumerator.MoveNext())
+//        {
+//            if (func(enumerator.Current))
+//            {
+//                yield return enumerator.Current;
+//            }
+//        }
+//    }
+//}}
