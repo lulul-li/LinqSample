c5ef607e24b7048c638a3f557941245cb4eb2d18
lulu.li29 <s987337613@gmail.com>
2018-09-19 00:36:01 +0800

    prepare training

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,7 +1,9 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using NSubstitute.Core;
 
 namespace LinqTests
 {
@@ -21,14 +23,101 @@ namespace LinqTests
 
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+        [TestMethod]
+        public void Any()
+        {
+            var products = RepositoryFactory.GetProducts();
+            Assert.IsTrue(products.myAny());
+        }
+
+        [TestMethod]
+        public void AnyWithPredicate()
+        {
+            var products = RepositoryFactory.GetProducts();
+            Assert.IsTrue(products.myAny(x => x.Price > 200));
+        }
+
+        [TestMethod]
+        public void All()
+        {
+            var products = RepositoryFactory.GetProducts();
+            Assert.IsFalse(products.myAll(current => current.Price > 200));
+
+        }
+        [TestMethod]
+        public void FirstOrDefault()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.myFirstOrDefault();
+        }
+        [TestMethod]
+        public void FirstOrDefault_Predicate()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.myFirstOrDefault(c => c.Price > 200);
+        }
     }
 }
 
-internal class WithoutLinq
+internal static class WithoutLinq
 {
     public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
     {
         throw new System.NotImplementedException();
+    }
+
+    public static bool myAny<TSource>(this IEnumerable<TSource> products)
+    {
+        return products.GetEnumerator().MoveNext();
+    }
+
+    public static bool myAny<TSource>(this IEnumerable<TSource> products, Func<TSource, bool> func)
+    {
+        var enumerator = products.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            if (func(enumerator.Current))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool myAll(this IEnumerable<Product> products, Func<Product, bool> predicate)
+    {
+        var enumerator = products.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            if (predicate(enumerator.Current))
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+    public static TSource myFirstOrDefault<TSource>(this IEnumerable<TSource> products)
+    {
+        var enumerator = products.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+
+            return enumerator.Current;
+
+        }
+        return default(TSource);
+    }
+    public static TSource myFirstOrDefault<TSource>(this IEnumerable<TSource> products, Func<TSource, bool> prediacte)
+    {
+        var enumerator = products.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            if (prediacte(enumerator.Current))
+            {
+                return enumerator.Current;
+            }
+        }
+        return default(TSource);
     }
 }
 
