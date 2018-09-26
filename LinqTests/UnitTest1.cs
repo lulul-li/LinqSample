c77652bb223633dbfd66897d955c3e30baa92933
lulu.li29 <s987337613@gmail.com>
2018-09-26 22:46:54 +0800

    add mySingle

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace LinqTests
     [TestClass]
     public class UnitTest1
     {
+        [Ignore]
         [TestMethod]
         public void find_products_that_price_between_200_and_500()
         {
@@ -18,7 +19,9 @@ namespace LinqTests
 
             var expected = new List<Product>()
             {
-                //todo
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -48,13 +51,42 @@ namespace LinqTests
         public void FirstOrDefault()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = products.myFirstOrDefault();
+            Assert.AreEqual(1, products.myFirstOrDefault().Id);
         }
         [TestMethod]
         public void FirstOrDefault_Predicate()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = products.myFirstOrDefault(c => c.Price > 200);
+            Assert.AreEqual(2,products.myFirstOrDefault(c => c.Price > 200).Id);
+        }
+        [TestMethod]
+        public void Single()
+        {
+            var products = RepositoryFactory.GetProducts();
+            Assert.AreEqual(110,products.mySingle(p=>p.Price<200).Price);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Single_Exception()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.mySingle(p => p.Price <0);
+        }
+
+        [TestMethod]
+        public void Distinct()
+        {
+            var expected = new List<Employee>
+            {
+                new Employee {Name = "Joe", Role = RoleType.Engineer, MonthSalary = 100, Age = 44, WorkingYear = 2.6},
+                new Employee {Name = "Kevin", Role = RoleType.Manager, MonthSalary = 380, Age = 55, WorkingYear = 2.6},
+                new Employee {Name = "Andy", Role = RoleType.OP, MonthSalary = 80, Age = 22, WorkingYear = 2.6},
+            };
+
+            var products = RepositoryFactory.GetEmployees();
+            WithoutLinq.myDistinct(products);
         }
     }
 }
@@ -118,6 +150,37 @@ internal static class WithoutLinq
             }
         }
         return default(TSource);
+    }
+
+    public static TScourec mySingle<TScourec>(this IEnumerable<TScourec> products, Func<TScourec, bool> predicate)
+    {
+        var enumerator = products.GetEnumerator();
+        var isMatch = false;
+        var singleData = default(TScourec);
+        while (enumerator.MoveNext())
+        {
+            if (predicate(enumerator.Current))
+            {
+                if (isMatch)
+                {
+                    throw new InvalidOperationException();
+                }
+                isMatch = true;
+                singleData = enumerator.Current;
+            }
+           
+        }
+        if (!isMatch)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return singleData;
+    }
+
+    public static void myDistinct(IEnumerable<Employee> products)
+    {
+        throw new NotImplementedException();
     }
 }
 
